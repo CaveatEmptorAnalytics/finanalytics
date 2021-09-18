8ec32c8b22e8c63eb90f9da9b8d3be85f5f67b97
Brennan <mhszeto.2019@sis.smu.edu.sg>
2021-09-18 22:08:14 +0800

    add folders

--- data/get/treasury.py
+++ data/get/treasury.py
@@ -1,4 +1,3 @@
-
 import re
 import csv
 import requests
@@ -6,7 +5,8 @@ import pandas as pd
 from bs4 import BeautifulSoup
 
 def get_rf_data():
-    url='https://www.treasury.gov/resource-center/data-chart-center/interest-rates/Pages/TextView.aspx?data=billratesAll'#Create a handle, page, to handle the contents of the website
+    #url will be changed according to the nubmer of years
+    url='https://www.treasury.gov/resource-center/data-chart-center/interest-rates/Pages/TextView.aspx?data=billratesAll'
     mostActiveStocksUrl = url
     page = requests.get(mostActiveStocksUrl)
     data = page.text
@@ -16,4 +16,3 @@ def get_rf_data():
     df = pd.read_html(str(rows))[0]
     return df
 
-    
